5f3c311589d85058abebf2ab5a7060484c9e7aea
Ander Gonzalez <andergonzaleztome@gmail.com>
2016-05-19 00:42:53 +0200

    ya funciona mejor y se puede ejecutar el .jar falta probar en rbpi

--- src/Conversor.java
+++ src/Conversor.java
@@ -115,18 +115,10 @@ public class Conversor implements SerialPortEventListener {
 	        	 System.out.println("Desconectado");
 	        	 serverInputStream = null;
 		         serverOutputStream = null;
+		         break;
 	         }catch(SocketTimeoutException s)
 	         {
 	            System.out.println("Socket timed out!");
-	            try {
-	            	serverOutputStream.close();
-					serverInputStream.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-	           
-	           // break;
 	         }catch(IOException e)
 	         {
 	            e.printStackTrace();
